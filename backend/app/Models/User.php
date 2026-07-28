0afc09e2e166060115cd786259d9c9d874df1184
NP-VAMS Setup <dev@npvams.local>
2026-07-28 14:59:06 +0545

    Fix: User model missing Authorizable trait

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -4,23 +4,25 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-use App\Support\Tenancy\BelongsToTenant;
+]use App\Support\Tenancy\BelongsToTenant;
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Activitylog\LogOptions;
 use Spatie\Activitylog\Traits\LogsActivity;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Model implements AuthenticatableContract
+class User extends Model implements AuthenticatableContract, AuthorizableContract
 {
-    use Authenticatable, BelongsToTenant, HasApiTokens, HasFactory, HasRoles, HasUuids, LogsActivity, Notifiable, SoftDeletes;
+    use Authenticatable, Authorizable, BelongsToTenant, HasApiTokens, HasFactory, HasRoles, HasUuids, LogsActivity, Notifiable, SoftDeletes;
 
     protected $guard_name = 'web';
 
